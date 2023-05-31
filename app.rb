9d3795f6afdc167a758176b72f5084e722032b02
PegahNa <pegah_uk@yahoo.co.uk>
2023-05-31 15:58:00 +0100

    test_driving_route_with_database

--- app.rb
+++ app.rb
@@ -8,9 +8,29 @@ require_relative 'lib/artist_repository'
 DatabaseConnection.connect
 
 class Application < Sinatra::Base
-  configure :development do
-    register Sinatra::Reloader
-    also_reload 'lib/album_repository'
-    also_reload 'lib/artist_repository'
-  end
+        configure :development do
+          register Sinatra::Reloader
+          also_reload 'lib/album_repository'
+          also_reload 'lib/artist_repository'
+        end
+
+        get '/albums' do
+          repo = AlbumRepository.new
+          albums = repo.all
+          response = albums.map do |album|
+          album.title
+          end.join(', ')
+          return response
+        end
+
+        post '/albums' do
+          repo = AlbumRepository.new
+          new_album = Album.new 
+          new_album.title = params[:title]
+          new_album.release_year = params[:release_year]
+          new_album.artist_id = params[:artist_id]
+
+          repo.create(new_album)
+          return ''
+        end
 end
--- spec/integration/application_spec.rb
+++ spec/integration/application_spec.rb
@@ -10,4 +10,31 @@ describe Application do
   # class so our tests work.
   let(:app) { Application.new }
 
+  context 'GET /albums' do
+    it 'should return the list of albums' do
+      response = get('/albums')
+
+      expected_response = 'Surfer Rosa, Waterloo, Super Trouper, Bossanova, Lover, Folklore, I Put a Spell on You, Baltimore, Here Comes the Sun, Fodder on My Wings, Ring Ring'
+
+      expect(response.status).to eq(200)
+      expect(response.body).to eq(expected_response)
+    end
+
+      context 'POST /albums' do
+        it 'should create a new album' do
+          response = post(
+            '/albums', title: 'OK Computer',
+             release_year: '1997',
+              artist_id: '1'
+            )
+          expect(response.status).to eq(200)
+          expect(response.body).to eq('') 
+       
+
+      response = get('/albums')
+
+      expect(response.body).to include('OK Computer')
+    end
+  end
+end
 end
